355ee4f17f728929f24ca52411caa823fd814bae
tranvanthanh99 <thanhvantran99@gmail.com>
2018-12-10 00:37:58 +0700

    update

--- src/game/Settings.java
+++ src/game/Settings.java
@@ -4,4 +4,5 @@ public class Settings {
     public static int SCREEN_WIDTH = 600, SCREEN_HEIGHT = 600;
     public static int PLAYER_POSITION_X = 200, PLAYER_POSITION_Y = 300;
     public static int PLAYER_WIDTH = 32, PLAYER_HEIGHT = 48;
+    public static int ENEMY_WIDTH = 32, ENEMY_HEIGHT = 48;
 }
--- src/game/enemy/Enemy.java
+++ src/game/enemy/Enemy.java
@@ -1,16 +1,22 @@
 package game.enemy;
 
+import game.Background;
 import game.GameObject;
+import game.Settings;
+import tklibs.Mathx;
 import tklibs.SpriteUtils;
 import game.renderer.Animation;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class Enemy extends GameObject {
+    Background background;
+    boolean checkX = false, checkY = false;
 
     public Enemy() {
         super();
         this.position.set(50, 50);
+        this.background = new Background();
         this.createRenderer();
     }
 
@@ -26,5 +32,49 @@ public class Enemy extends GameObject {
         images.add(SpriteUtils.loadImage("assets/images/enemies/level0/black/8.png"));
         this.renderer = new Animation(images);
 
+    }
+
+    public void run() {
+
+        if (this.position.x == 16) {
+            checkX = true;
+        }
+        else if (this.position.x == 368) {
+            checkX = false;
+        }
+
+        if (this.position.y == 570) {
+            checkY = true;
+        }
+        else if (this.position.y == 25){
+            checkY = false;
+        }
+
+        if (checkX) {
+            this.position.addThis(3,0);
+        }
+        else {
+            this.position.addThis(-3,0);
+        }
+
+        if (checkY) {
+            this.position.addThis(0 , -0.5f);
+        }
+        else {
+            this.position.addThis(0 , 0.5f);
+        }
+
+        this.limitEnemyPosition();
+    }
+
+    private void limitEnemyPosition() {
+        BufferedImage image = SpriteUtils.loadImage("assets/images/players/straight/0.png");
+        int halfWidth = (int) (Settings.ENEMY_WIDTH *  this.anchor.x);
+        int halfHeight = (int) (Settings.ENEMY_HEIGHT *  this.anchor.y);
+
+        float x = (float) Mathx.clamp(this.position.x, halfWidth, background.image.getWidth() - halfWidth);
+
+        float y = (float) Mathx.clamp(this.position.y, halfHeight, Settings.SCREEN_HEIGHT - halfHeight);
+        this.position.set(x, y);
     }
 }
